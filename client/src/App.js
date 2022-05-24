d4ac5e8a97fbf93466044f05907e302b339cdc9c
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2022-05-24 21:33:54 +0300

    finish

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    ContractData
+} from '@drizzle/react-components';
+import Land from './artifacts/Land.json';
+import './index.css';
+
+
+const drizzleOptions = {
+    contracts: [Land]
+}
+
+var rows = [];
+for (var i = 1; i < 5; i++) {
+    rows.push(<ContractData contract="Land" method="getArea" methodArgs={[i, { from: "0xe0786c396EEf88a42782cfd63D2329cA14b62C4C"}]} />);
+}
+
+function App() {
+    return (
+    <></>
+    );
+}
+export default App;
